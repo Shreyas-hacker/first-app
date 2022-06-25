5c0b68e70f95c79c3ec8f0e50b8d52cd3a7cf810
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2022-06-25 20:31:26 +0800

    CSS Added

--- src/index.js
+++ src/index.js
@@ -8,21 +8,29 @@ import './index.css';
 //stateless functional componnent
 function BookList(){
     return (
-        <section>
+        <section className="bookList">
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
             <Book/>
         </section>
     );
 }
 //Nested Components
 const Book = () => {
-    return <article>
+    return <article className="book">
         <Image/>
         <Title/>
         <Author/>
     </article>
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71Mx-bE91+L._AC_UL604_SR604,400_.jpg" alt=""/>
-const Title = () => <h1>Its not summer without you</h1>
-const Author = () => <h4>Jenny Han</h4>
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg" alt=""/>
+const Title = () => <h1>I Love You to the Moon and Back</h1>
+const Author = () => <h4 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>Amelia Hepworth</h4>
 ReactDom.render(<BookList/>,document.getElementById('root'))
