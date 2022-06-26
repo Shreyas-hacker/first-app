cb2dc207fe77f0e3f7f52debee5376038a6b0ad9
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2022-06-27 01:59:50 +0800

    Props

--- src/index.js
+++ src/index.js
@@ -4,33 +4,30 @@ import ReactDom from 'react-dom'
 
 //CSS
 import './index.css';
+//setup vars
+const firstBook = {
+    img:"https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth"
+}
 
 //stateless functional componnent
 function BookList(){
     return (
         <section className="bookList">
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
-            <Book/>
+            <Book img = {firstBook.img} title = {firstBook.title} author = {firstBook.author}/>
         </section>
     );
 }
 //Nested Components
-const Book = () => {
-    return <article className="book">
-        <Image/>
-        <Title/>
-        <Author/>
-    </article>
+const Book = (props) => {
+    return (
+        <article className="book">
+            <img src={props.img} alt=""/>
+            <h1>{props.title}</h1>
+            <h4>{props.author.toUpperCase()}</h4>
+        </article>
+    );
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg" alt=""/>
-const Title = () => <h1>I Love You to the Moon and Back</h1>
-const Author = () => <h4 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>Amelia Hepworth</h4>
 ReactDom.render(<BookList/>,document.getElementById('root'))
