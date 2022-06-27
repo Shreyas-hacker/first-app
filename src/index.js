f48e6e06f948c3ca89e71ea6fa911897f3f0e5c0
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2022-06-27 10:55:14 +0800

    new syntax for set up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react"
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 //CSS
 import './index.css';
 //setup vars
 const books = [
     {
+        id:1,
         img:"https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg",
         title: "I Love You to the Moon and Back",
         author: "Amelia Hepworth"
     },
     {
+        id:2,
         img:"https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L._AC_UL604_SR604,400_.jpg",
         title: 'The Subtle Art of Not Giving A Fuck',
         author: 'Mark Manson'
@@ -23,20 +25,22 @@ function BookList(){
     return (
         <section className="bookList">
             {books.map((book)=>{
-                return <Book img={book.img} title={book.title} author={book.author}/>
+                return <Book key={book.id} book={book}/>
             })}
         </section>
     );
 }
 //Nested Components
 const Book = (props) => {
+    const {img,title,author} = props.book
     return (
         <article className="book">
-            <img src={props.img} alt=""/>
-            <h1>{props.title}</h1>
-            <h4>{props.author.toUpperCase()}</h4>
+            <img src={img} alt=""/>
+            <h1>{title}</h1>
+            <h4>{author.toUpperCase()}</h4>
         </article>
     );
 }
 
-ReactDom.render(<BookList/>,document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<BookList/>)
