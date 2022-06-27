0052279658b0b69f06fbf653eb1a1b298248cddb
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2022-06-27 10:44:39 +0800

    More Props and img css

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ import ReactDom from 'react-dom'
 //CSS
 import './index.css';
 //setup vars
-const firstBook = {
-    img:"https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg",
-    title: "I Love You to the Moon and Back",
-    author: "Amelia Hepworth"
-}
+const books = [
+    {
+        img:"https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg",
+        title: "I Love You to the Moon and Back",
+        author: "Amelia Hepworth"
+    },
+    {
+        img:"https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L._AC_UL604_SR604,400_.jpg",
+        title: 'The Subtle Art of Not Giving A Fuck',
+        author: 'Mark Manson'
+    }
+];
 
 //stateless functional componnent
 function BookList(){
     return (
         <section className="bookList">
-            <Book img = {firstBook.img} title = {firstBook.title} author = {firstBook.author}/>
+            {books.map((book)=>{
+                return <Book img={book.img} title={book.title} author={book.author}/>
+            })}
         </section>
     );
 }
